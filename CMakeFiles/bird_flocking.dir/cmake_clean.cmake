8119d491285e20ed96bf6f48271c363d2bfd035c
Avisenna Abimanyu <theknightmare07@gmail.com>
2020-03-21 06:56:45 +0700

    OpenGL files

---
+++ CMakeFiles/bird_flocking.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bird_flocking.dir/src/example1.cpp.o"
+  "CMakeFiles/bird_flocking.dir/src/InitShader.cpp.o"
+  "bird_flocking.pdb"
+  "bird_flocking"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bird_flocking.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
